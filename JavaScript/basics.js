b17f5c4e4a5cca18f5d98bd97c35fcd9ca9b688b
mobashshirhasan <mobashshir12@gmail.com>
2025-11-14 20:02:11 +0530

    Refactor JavaScript basics.js to comment out console API examples and variable declarations for clarity

--- JavaScript/basics.js
+++ JavaScript/basics.js
@@ -4,9 +4,42 @@
 // document.write("this is document write")
 
 // Javascript console API (Application Program Interface):
-console.log("hello world", 4+6, "Another log"); 
-console.warn("this is warning");
-console.error("this is an error");
+// console.log("hello world", 4+6, "Another log"); 
+// console.warn("this is warning");
+// console.error("this is an error");
 
 // JavaScript Variables
+
+// what are varables :- containers to store data values
+
+// var num1 = 20;
+// var num2 = 50.02;
+// console.log(num1 + num2);
+
+
+// Data Types :
+
+// 1 Numbers :
+// var num1 = 20;
+// var num2 = 50.02;
+
+
+// Strings : 
+// var str1 = "This is string."
+// var str2 = "This is also a string."
+
+
+// Objects : (key : value)
+
+// var marks = {
+//     ravi : 50,
+//     shubham : 60,
+//     harry : 70
+// }
+
+
+// Booleans :
+
+// var a = true;
+// var b = false;
 
