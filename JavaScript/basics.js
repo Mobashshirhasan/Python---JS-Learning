131319769d285385a4982002637fc5219f02fdd7
mobashshirhasan <mobashshir12@gmail.com>
2025-11-14 21:32:57 +0530

    Add detailed comments on primitive and non-primitive data types, operators, and functions in basics.js for better clarity

--- JavaScript/basics.js
+++ JavaScript/basics.js
@@ -43,3 +43,69 @@
 // var a = true;
 // var b = false;
 
+
+// Primitive Data Types [Immutable]: (Srtings, Numbers, Boolean, Symbol, Null, Undefined, BigInt)
+// Non Primitive Data Types [Mutable]: (Objects, Arrays, Function)
+
+// Key Differences (Primitive vs Non-Primitive)
+// Primitive	            Non-Primitive
+// Stored by value	        Stored by reference
+// Immutable	            Mutable
+// Fast, simple data	    Complex data structures
+// Size fixed	            Size variable
+// 7 types	                Object, Array, Function
+
+
+// Operators
+
+// Arithmetic Operators : 
+// + , - , * , / , 
+
+// Assignment Operators : 
+// = , += , -= , *= , /=
+
+// comparison Operators : 
+// == , >= , <= , < , >
+
+
+// Logical AND Operators : (It will not return true until both values are true....)
+
+// true && true    //true
+// true && false   //false
+// false && true   //flase
+// false && false  //false
+
+
+// Logical OR Operators : (It will give ture ,if any one of the value is true...)
+
+// true || true    //true
+// true || false   //true
+// false || true   //true
+// false || false  //false
+
+// Logical NOT : 
+// !false    //true
+// !true       //false
+
+
+// FUNCTIONS : 
+
+// function avg(a, b){
+//     return(a + b) / 2;
+// }
+
+// c1 = avg(4, 6);
+// c2 = avg(14, 16);
+// console.log(c1, c2);
+
+
+//  Write a function that takes a name and prints "Hello <name>"...
+
+function greet(name){
+    a = "hello Mobashshir"
+    return a;
+}
+
+result = greet();
+console.log(result);
+
