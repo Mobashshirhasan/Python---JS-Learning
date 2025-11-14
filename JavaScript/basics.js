ef04a8422bf0fd7850fba6cbf1c1b89dadaf6cfd
mobashshirhasan <mobashshir12@gmail.com>
2025-11-14 09:31:06 +0530

    Add initial implementation of JavaScript basics and string manipulation examples in Jupyter notebook

---
+++ JavaScript/basics.js
@@ -0,0 +1,12 @@
+// ways to priont in JavaScript
+// console.log("hello world");
+// alert("me");
+// document.write("this is document write")
+
+// Javascript console API (Application Program Interface):
+console.log("hello world", 4+6, "Another log"); 
+console.warn("this is warning");
+console.error("this is an error");
+
+// JavaScript Variables
+
