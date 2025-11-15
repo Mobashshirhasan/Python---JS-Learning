a640ac08f92ba0541bcede56e030cca6b284e4ee
mobashshirhasan <mobashshir12@gmail.com>
2025-11-15 18:08:40 +0530

    Add initial HTML structure and JavaScript for TODO list application

---
+++ JavaScript/DOM/index.js
@@ -0,0 +1,8 @@
+// DOM Manipulation
+
+let element = document.getElementById('click');
+console.log(element);
+
+let elementClass = document.getElementsByClassName('container')
+console.log(elementClass);
+elementClass[0].style.background = 'yellow'
