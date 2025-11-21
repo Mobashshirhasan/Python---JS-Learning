3a7044ab020db49997fe29055da6ccee1dd2ef99
mobashshirhasan <mobashshir12@gmail.com>
2025-11-21 21:59:34 +0530

    Add initial variable declarations and logging in basics.js

---
+++ JavaScript/practice/01_basics.js
@@ -0,0 +1,14 @@
+//  Variables 
+
+const accountId = 1234
+let accountEmail = "mobashshir12@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState
+
+console.table({ accountId, accountEmail, accountPassword, accountCity, accountState });
+
+accountEmail = "mustafa@microsoft.com"
+accountPassword = "New Password"
+accountCity = "New Delhi"
+
