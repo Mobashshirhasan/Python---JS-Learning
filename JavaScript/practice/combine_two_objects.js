9753fa7cb12eb873d324b4d30af90ab32e24864f
mobashshirhasan <mobashshir12@gmail.com>
2025-12-02 21:30:58 +0530

    using hasOwnProperty

--- JavaScript/practice/combine_two_objects.js
+++ JavaScript/practice/combine_two_objects.js
@@ -13,7 +13,11 @@ Example:
 let myObj1 = { a: 1, b: 2 }
 let myObj2 = { b: 3, c: 4 }
 
+let myArr1 = Object.entries(myObj1)
+let myArr2 = Object.entries(myObj2)
 
-let newObj = { ...myObj1, ...myObj2 }
+let newObj = myArr1.filter(([k]) => !myObj2.hasOwnProperty(k)).concat(myArr2)
 
 console.log(newObj);
+console.log(myArr1);
+console.log(myArr2);
